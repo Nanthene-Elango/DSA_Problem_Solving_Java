4a2c853d63aae2ae0665b264dce4cb596c27bf9a
Nanthene-Elango <nantheneelango@gmail.com>
2025-08-14 13:26:13 +0530

    solved problem in Pattern

---
+++ Patterns/SquareNumbers.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class SquareNumbers {
+    
+    public static void main(String args[]){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the value of n: ");
+        int n = input.nextInt();
+
+        int size = 2*n-1;
+
+        for (int i = 0 ; i < size ; i++){
+            for (int j = 0 ; j < size ; j++){
+
+                int top = i;
+                int bottom = j;
+                int left = size-1-i;
+                int right = size-1-j;
+
+                int mindist = Math.min(Math.min(right, bottom), Math.min(top, left));
+                System.out.print(n-mindist);
+            }
+            System.out.println();
+        }
+
+        input.close();
+    }
+}
+
+// Write a program to print the below series.
+
+// Input: 4
+
+// 4444444
+
+// 4333334
+
+// 4322234
+
+// 4321234
+
+// 4322234
+
+// 4333334
+
+// 4444444
